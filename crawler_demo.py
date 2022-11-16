d966600bd5cd8da369e8b769301bd952813554ca
Simon <simon_wzm@163.com>
2022-11-17 01:15:51 +0800

    complete producers

--- crawler_demo.py
+++ crawler_demo.py
@@ -1,4 +1,5 @@
 import newspaper
+import time
 import requests
 import utils
 import datetime
@@ -15,9 +16,50 @@ from newspaper import network
 from newspaper import settings
 from newspaper import mthreading
 from newspaper import outputformatters
+import queue
+import threading
+import json
 
 
+q = queue.Queue()
+def producer(urlList, session):
+    for url in urlList:
+        try:
+            date = url[-1:-9:-1][::-1]
+            time.sleep(0.5)
+            response = session.get(url=url, proxies=PROXIES)
+            # result_item = json.loads(response.text).get('items')[0]
+            result_item = json.loads(response.text).get('items')
+            for item in result_item:
+                if len(str(item[0]))==6 and item[1]=='200':
+                    result = item
+                    break
+                else:
+                    continue
 
+            processed_url = utils.clean_url(item, date)
+            q.put(processed_url)
+        except Exception as e:
+            print(e,'\n', url)
+            continue
+    
+def consumer(session):
+    while True:
+        if q.get == None:
+            q.task_done()
+            break
+        try:
+            url = q.get()
+            response = session.get(url=url, proxies=PROXIES, allow_redirects=False)
+            soup = bs4(response.text, 'html.parser')
+            #change url string into valid filename
+            filename = "".join(i for i in url if i not in "\/:*?<>|") + '.html' 
+            with open(f'./html/{filename}', 'wb') as f:
+                f.write(soup.prettify().encode('utf-8'))
+            q.task_done()
+        except Exception as e:
+            print(e)
+            continue
 
 config  = Config()
 config.browser_user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36'
@@ -30,15 +72,45 @@ config.request_timeout = 20
 
 
 BASE_URL = 'https://web.archive.org/web/20190501000000*/cnn.com/business/tech'
+
+
+
+
 session  = requests.Session()
 session.headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36'}
 session.timeout = 20
-session.verify = False
+
+session2 = requests.Session()
+session.headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36'}
+session.timeout = 100
+# session.verify = False
 response = session.get(url=BASE_URL, proxies=PROXIES)
 template_url = 'https://web.archive.org/__wb/calendarcaptures/2?url=cnn.com%2Fbusiness%2Ftech&date='
 
 date_str_list = utils.get_date_str_list(datetime.date(2019, 1, 1), datetime.date(2019, 12, 31))
 base_url_list = [template_url+str(date) for date in date_str_list]
+#split list into 12 slices
+base_url_list_list = [base_url_list[i:i+12] for i in range(0, len(base_url_list), 12)]
+# print(base_url_list_list)
+
+for i in range(12):
+    t = threading.Thread(target=producer, args=(base_url_list_list[i], session))
+    t.start()
+
+# for i in range(4):
+#     c = threading.Thread(target=consumer, args=(session2,))
+#     c.start()
+
+t.join()
+for i in range(4):
+    q.put(None)
+
+# c.join()
+
+
+
+
+
 
 
 
--- utils.py
+++ utils.py
@@ -7,4 +7,13 @@ def get_date_str_list(date_start:datetime.date, date_end:datetime.date)->list:
     date_str_list = [date.strftime('%Y%m%d') for date in date_list]
     return date_str_list
 
-print(get_date_str_list(datetime.date(2019, 1, 1), datetime.date(2019, 12, 31)))
+def clean_url(token_string_list, date_string):
+    #convert each item in list to string
+    # date_string_list = [str(date) for date in date_string_list]
+    #concatenate each item in list
+    # date_string = ''.join(date_string_list)
+    token = token_string_list[0] 
+    print(token_string_list)
+    template_url = f'https://web.archive.org/web/{date_string}{token}/cnn.com/business/tech'
+    return template_url
+# clean_url([130328, 200, 1])
